e05f4e3e76a085fd49333b49d679e425f671ea8b
Suryansh Singh <141821325+suryxnshsingh@users.noreply.github.com>
2025-05-29 17:58:34 +0530

    feature section tabs

--- src/assets/components/landnew/land.jsx
+++ src/assets/components/landnew/land.jsx
@@ -1,17 +1,21 @@
 
 import { HeroSectionOne } from '../ui/hero'
+import { TabsDemo } from '../ui/tabsDemo'
 import FooterFull from './foot'
 import { Navbar } from './navbar'
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black relative overflow-hidden">
+      {/* Background gradient overlay */}
+      <div className="absolute inset-0 bg-gradient-to-b from-black via-gray-900/50 to-black"></div>
+      
       <Navbar />
-      <div className="pt-20 flex flex-col items-center">
+      <div className="relative z-10 pt-20 flex flex-col items-center">
         <div className="w-full max-w-7xl">
           <HeroSectionOne/>
+          <TabsDemo/>
         </div>
-        {/* Add your main content here */}
         <div className="w-full">
           <FooterFull/>
         </div>
